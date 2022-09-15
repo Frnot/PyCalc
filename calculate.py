3db9bd8f4f91add858088bf66bc3cd2602eb5753
Frnot <39629813+Frnot@users.noreply.github.com>
2022-09-15 12:09:09 -0500

    v0.5

--- calculate.py
+++ calculate.py
@@ -1,9 +1,8 @@
-#v0.4
+#v0.5
 
 ## TODO:
-# open another window (GUI) to display defined variables
-# store variables persistently
-#   add clear function to clear stored variables
+# Add info to start screen (overwrite ipython text)
+# Add more math functions
 
 import sys
 from time import time
@@ -21,17 +20,21 @@ def define_constants():
     Sau = 4.098e7
     Sag = 6.173e7
 
-    # configuration variables
-    precision = 5
-
     globals().update(locals())
 
 
 def define_functions():
     # Common Functions
-    log = lambda x: ln(x, 10)
-    log10 = lambda x: log(x)
+    log10 = lambda x: ln(x, 10)
+    log2 = lambda x: ln(x, 2)
 
+    mag2db = lambda x: sp.N(20*log10(x))
+    pow2db = lambda x: sp.N(10*log10(x))
+    db2mag = lambda x: 10**(x/20)
+    db2pow = lambda x: 10**(x/10)
+
+    reflection = lambda z0, zl: (zl - z0)/(zl + z0)
+    transmission = lambda z0, zl: (2 * zl)/(zl + z0)
     SWR = lambda gamma: (1+abs(gamma))/(1-abs(gamma))
     ZatD = lambda z0, zl, B, l: sp.N(z0*(zl+1j*z0*tan(B*l))/(z0+1j*zl*tan(B*l)))
 
@@ -44,7 +47,10 @@ def load_dependencies():
     #autoinstall("import matplotlib")
 
     from IPython import get_ipython
-    from sympy import pi,sin,cos,tan,log,ln
+    from sympy import (pi,log,ln)
+    from sympy import (sin,sinc,cos,tan,cot,sec,csc,asin,acos,atan,acot,asec,acsc)
+    from sympy import (sinh,cosh,tanh,coth,sech,csch,asinh,acosh,atanh,acoth,asech,acsch)
+    #from sympy import (angle, atan2, conj, imag, nthroot, real, sign, signIm)
 
     globals().update(locals())
 
@@ -53,6 +59,8 @@ def load_dependencies():
 def main():
     start = time()
     print("Intializing.", end="", flush=True)
+
+    precision = 5
 
     load_dependencies()
     define_constants()
